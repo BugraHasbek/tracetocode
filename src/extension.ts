69b38b175f27bc3e07c0a28b7baa24da564de66a
Bugra Hasbek <bugrahasbek@gmail.com>
2021-02-21 20:41:22 +0300

    Create .tracetocode folder for storing extension related stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,24 @@
 import * as vscode from 'vscode';
+import { access, constants, mkdirSync, existsSync } from 'fs';
+const foldername = '.tracetocode';
+
+function initialize(){
+	vscode.window.showInformationMessage('tracetocode active!');	
+}
+
+function createRequirement(){
+	if(!vscode.workspace.workspaceFolders){
+		vscode.window.showInformationMessage('A project folder has not been opened yet!');
+		return;
+	}
+
+	var absolutePath = vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + foldername;
+	access(absolutePath, constants.F_OK, (err) => {
+		if(err){
+			mkdirSync(absolutePath);
+		}
+	});
+}
 
 export function activate(context: vscode.ExtensionContext) {
 	let create = vscode.commands.registerCommand('tracetocode.createRequirement', createRequirement);
@@ -8,10 +28,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function deactivate() {}
 
-function initialize(){
-	vscode.window.showInformationMessage('Initializing .tracetocode folder');	
-}
-
-function createRequirement(){
-	vscode.window.showInformationMessage('Creating a new software requirement');
-}

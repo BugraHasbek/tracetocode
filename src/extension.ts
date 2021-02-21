abe401faff8cb7c7769779bd19c0598e11a54ef7
Bugra Hasbek <bugrahasbek@gmail.com>
2021-02-21 21:04:17 +0300

    Added input box for requirements

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,37 @@
-import * as vscode from 'vscode';
+import {window, workspace, commands, ExtensionContext, QuickInputButtons} from 'vscode';
 import { access, constants, mkdirSync, existsSync } from 'fs';
 const foldername = '.tracetocode';
 
 function initialize(){
-	vscode.window.showInformationMessage('tracetocode active!');	
+	window.showInformationMessage('tracetocode active!');	
 }
 
 function createRequirement(){
-	if(!vscode.workspace.workspaceFolders){
-		vscode.window.showInformationMessage('A project folder has not been opened yet!');
+	if(!workspace.workspaceFolders){
+		window.showInformationMessage('A project folder has not been opened yet!');
 		return;
 	}
 
-	var absolutePath = vscode.workspace.workspaceFolders[0].uri.fsPath + '/' + foldername;
+	var absolutePath = workspace.workspaceFolders[0].uri.fsPath + '/' + foldername;
 	access(absolutePath, constants.F_OK, (err) => {
 		if(err){
 			mkdirSync(absolutePath);
 		}
 	});
+
+	window.showInputBox().then(value => {
+		if(value)
+			window.showInformationMessage('Requirement is :' + value);	
+
+		/// keep a persistent id and create requirements based on this id. 
+		/// 1 file per requirement
+		/// 1 map file for tracing requirements to code
+	});
+	
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	let create = vscode.commands.registerCommand('tracetocode.createRequirement', createRequirement);
+export function activate(context: ExtensionContext) {
+	let create = commands.registerCommand('tracetocode.createRequirement', createRequirement);
 	context.subscriptions.push(create);
 	initialize();
 }

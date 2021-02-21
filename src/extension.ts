5ff0c5128f1dc1f76de39bb9e8790a2af30b1a02
Bugra Hasbek <bugrahasbek@gmail.com>
2021-02-21 16:49:58 +0300

    moved lambdas to new functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let createRequirement = vscode.commands.registerCommand('tracetocode.createRequirement', () => {
-		vscode.window.showInformationMessage('Creating a new software requirement');
-	});
-
-	context.subscriptions.push(createRequirement);
-
-	vscode.window.showInformationMessage('Initializing .tracetocode folder');	
+	let create = vscode.commands.registerCommand('tracetocode.createRequirement', createRequirement);
+	context.subscriptions.push(create);
+	initialize();
 }
 
 export function deactivate() {}
+
+function initialize(){
+	vscode.window.showInformationMessage('Initializing .tracetocode folder');	
+}
+
+function createRequirement(){
+	vscode.window.showInformationMessage('Creating a new software requirement');
+}
